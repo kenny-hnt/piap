78dd0eaf252d14e8cb59a0c61ab05b72c447fb6a
Kenny <kenny@subthings.com>
2020-05-02 17:36:22 +0700

    backup file if not do it yet

--- piap.sh
+++ piap.sh
@@ -2,22 +2,24 @@
 
 _RED="\033[0;31m"
 _GREEN="\033[0;32m"
-_YELLOW="\033[0;33m"
-_RASPBERRY="\033[0;35m"
-_ERROR="\033[1;37;41m"
 _RESET="\033[m"
-
-
 
 function pp(){
 	echo -e -e "${_GREEN}$1...${_RESET}\n"
 }
 
+backupFile(){
+	targetFile=$1
+	if [ ! -f ${targetFile}.original ] ; then 
+		mv ${targetFile} ${targetFile}.original	
+	else
+		echo "file existed"
+	fi
+}
+
 unusedServices="systemd-networkd systemd-resolved isc-dhcp-server isc-dhcp-client"
 pp "Turning Off the unused services: ${unusedServices}"
 systemctl	disable $unusedServices > /dev/null 2>&1
-
-
 
 requiredService="wpa_supplicant dhcpcd dnsmasq hostapd"
 pp "Setup required services: ${requiredService}"
@@ -30,24 +32,24 @@ do
 done
 
 echo -e "\nSetup wpa_supplication..."
-mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.originial
+backupFile /etc/wpa_supplicant/wpa_supplicant.conf
 cp etc.wpa_supplicant.wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo -e "\nSetup network interfaces..."
-mv /etc/network/interfaces /etc/network/interfaces.original
+backupFile /etc/network/interfaces 
 cp etc.network.interfaces /etc/network/interfaces
 rfkill unblock wifi
 
 echo -e "\nSetup dhcpcd..."
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.original
+backupFile /etc/dhcpcd.conf
 cp etc.dhcpcd.conf /etc/dhcpcd.conf
 
 echo -e "\nSetup dnsmasq..."
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.original
+backupFile /etc/dnsmasq.conf 
 cp etc.dnsmasq.conf /etc/dnsmasq.conf
 
 echo -e "\nSetup hostapd..."
-mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.original
+backupFile /etc/hostapd/hostapd.conf 
 cp etc.hostapd.hostapd.conf /etc/hostapd/hostapd.conf 
 systemctl unmask hostapd
 systemctl enable hostapd
@@ -55,9 +57,8 @@ systemctl enable hostapd
 echo -e "\nSetup NAT..."
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 cp etc.iptables.ipv4.nat /etc/iptables.ipv4.nat
-mv /etc/rc.local /etc/rc.local.original
+backupFile /etc/rc.local 
 cp etc.rc.local /etc/rc.local
 
-
-echo -e "\n========== FINISHED ======"
+echo -e "\n========== FINISHED =========="
 echo -e "Please reboot"
